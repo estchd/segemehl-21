73b93789860c516fb30fd593a1d8f10d71067e24
erik <es40jeri@uni-leipzig.de>
2021-09-24 13:49:28 +0200

    Added more plots to test out the different ways of passing values to the box and violin plots

--- frontend/www/js/plots/test_plots.js
+++ frontend/www/js/plots/test_plots.js
@@ -1,33 +1,105 @@
 export function setup_test_plots() {
-    setup_test_box_plot();
-    setup_test_violin_plot()
+    setup_test_box_base_item_plot();
+    setup_test_box_box_plot_item_plot();
+    setup_test_box_ikde_item_plot();
+    setup_test_box_violin_item_plot();
+    setup_test_violin_base_item_plot();
+    setup_test_violin_box_plot_item_plot();
+    setup_test_violin_ikde_item_plot();
+    setup_test_violin_violin_item_plot();
 }
 
 export function update_test_plot() {
-    update_test_box_plot();
-    update_test_violin_plot();
+    update_test_box_base_item_plot();
+    update_test_box_box_plot_item_plot();
+    update_test_box_ikde_item_plot();
+    update_test_box_violin_item_plot();
+    update_test_violin_base_item_plot();
+    update_test_violin_box_plot_item_plot();
+    update_test_violin_ikde_item_plot();
+    update_test_violin_violin_item_plot();
 }
 
 export function update_reference_dependent_test_plots() {
 
 }
 
-let test_box_plot;
+let test_box_base_item_plot;
 
-function setup_test_box_plot() {
-
-}
-
-function update_test_box_plot() {
+function setup_test_box_base_item_plot() {
 
 }
 
-let test_violin_plot;
-
-function setup_test_violin_plot() {
+function update_test_box_base_item_plot() {
 
 }
 
-function update_test_violin_plot() {
+let test_box_box_plot_item_plot;
+
+function setup_test_box_box_plot_item_plot() {
+
+}
+
+function update_test_box_box_plot_item_plot() {
+
+}
+
+let test_box_ikde_item_plot;
+
+function setup_test_box_ikde_item_plot() {
+
+}
+
+function update_test_box_ikde_item_plot() {
+
+}
+
+let test_box_violin_item_plot;
+
+function setup_test_box_violin_item_plot() {
+
+}
+
+function update_test_box_violin_item_plot() {
+
+}
+
+let test_violin_base_item_plot;
+
+function setup_test_violin_base_item_plot() {
+
+}
+
+function update_test_violin_base_item_plot() {
+
+}
+
+let test_violin_box_plot_item_plot;
+
+function setup_test_violin_box_plot_item_plot() {
+
+}
+
+function update_test_violin_box_plot_item_plot() {
+
+}
+
+let test_violin_ikde_item_plot;
+
+function setup_test_violin_ikde_item_plot() {
+
+}
+
+function update_test_violin_ikde_item_plot() {
+
+}
+
+let test_violin_violin_item_plot;
+
+function setup_test_violin_violin_item_plot() {
+
+}
+
+function update_test_violin_violin_item_plot() {
 
 }
